a078c6e96802ee3adebf8698fed8ca4bfdc4f8f2
morrillo <gustavo.orrillo@moldeo.coop>
2015-08-27 12:34:17 -0300

    Changes

--- session_connector.py
+++ session_connector.py
@@ -46,37 +46,43 @@ class session_connector(osv.osv):
 	order_obj = self.pool.get('sale.order')
 	partner_obj = self.pool.get('res.partner')
 
-	conn = pymysql.connect(host=dict_parms['host'],port=dict_parms['port'],user=dict_parms['user'],\
+	conn = pymysql.connect(host=dict_parms['host'],user=dict_parms['user'],\
 				passwd=dict_parms['pwd'],db=dict_parms['dbname'])
+	#conn = pymysql.connect(host=dict_parms['host'],port=dict_parms['port'],user=dict_parms['user'],\
+	#			passwd=dict_parms['pwd'],db=dict_parms['dbname'])
 	cur = conn.cursor(pymysql.cursors.DictCursor)
 	
-	cur.execute("select from_unixtime(cdate) fecha,jos_vm_orders.* from jos_vm_orders")
+	cur.execute("select date(created_on) as fecha,vz3vx_virtuemart_orders.* from vz3vx_virtuemart_orders")
 	contador = 0
+	index = 0
 	for r in cur.fetchall():
+		print(index)
+		index = index + 1
 		order_id = 0
-		order_id = order_obj.search(cr,uid,[('virtuemart_order_id','=',int(r['order_id']))])
-		partner_id = partner_obj.search(cr,uid,[('virtuemart_user_id','=',r['user_info_id'])])
+		order_id = order_obj.search(cr,uid,[('virtuemart_order_id','=',int(r['virtuemart_order_id']))])
+		partner_id = partner_obj.search(cr,uid,[('virtuemart_user_id','=',r['virtuemart_user_id'])])
 
 		if not partner_id:
-			raise osv.except_osv("ERROR", "Problema de sincronización con maestro de clientes")
+			# raise osv.except_osv("ERROR", "Problema de sincronización con maestro de clientes")
+			print("ERROR Problema de sincronización con maestro de clientes")
+			continue
 
 		vals_order = {
-			'virtuemart_order_id': r['order_id'],
+			'virtuemart_order_id': r['virtuemart_order_id'],
 			'amount_untaxed': float(r['order_subtotal']),
 			'amount_tax': float(r['order_tax']),
 			'amount_total': float(r['order_total']),
 			'date_order': str(r['fecha']),
-			'note': r['customer_note'].decode('cp1252'),
+			'note': r['oc_note'].decode('cp1252'),
 			'partner_id': partner_id[0],
 			'pricelist_id': 1,
+			'picking_policy': 'direct',
+			'warehouse_id': 1,
 			'partner_invoice_id': partner_id[0],
 			'partner_shipping_id': partner_id[0],
 			}
 		if not order_id:
-			try:
-				order_id = order_obj.create(cr,uid,vals_order)
-			except:
-				raise osv.except_osv("ERROR", "No se pudo insertar una orden")
+			order_id = order_obj.create(cr,uid,vals_order)
 
 	_logger.info(str(date.today())+" - Interface VirtueMart - Fin carga ordenes")
 	conn.close()
@@ -89,18 +95,26 @@ class session_connector(osv.osv):
 	partner_obj = self.pool.get('res.partner')
 	product_obj = self.pool.get('product.product')
 	
-	conn = pymysql.connect(host=dict_parms['host'],port=dict_parms['port'],user=dict_parms['user'],\
+	conn = pymysql.connect(host=dict_parms['host'],user=dict_parms['user'],\
 				passwd=dict_parms['pwd'],db=dict_parms['dbname'])
+	#conn = pymysql.connect(host=dict_parms['host'],port=dict_parms['port'],user=dict_parms['user'],\
+	#			passwd=dict_parms['pwd'],db=dict_parms['dbname'])
 	cur = conn.cursor(pymysql.cursors.DictCursor)
 	
-	cur.execute("select * from jos_vm_order_item")
+	cur.execute("select * from vz3vx_virtuemart_order_items")
 	contador = 0
+	product_id_unknown = product_obj.search(cr,uid,[('default_code','=','UNKNOWN')])
 	# pdb.set_trace()
 	for r in cur.fetchall():
+		print contador
+		contador = contador + 1
 		order_id = 0
-		order_ids = order_obj.search(cr,uid,[('virtuemart_order_id','=',r['order_id'])])
-		order_line_ids = order_line_obj.search(cr,uid,[('virtuemart_order_id','=',r['order_item_id'])])
-		product_id = product_obj.search(cr,uid,[('virtuemart_product_id','=',int(r['product_id']))])
+		order_ids = order_obj.search(cr,uid,[('virtuemart_order_id','=',r['virtuemart_order_id'])])
+		order_line_ids = order_line_obj.search(cr,uid,[('virtuemart_order_id','=',r['virtuemart_order_item_id'])])
+		product_id = product_obj.search(cr,uid,[('virtuemart_product_id','=',int(r['virtuemart_product_id']))])
+
+		if not product_id:
+			product_id = product_id_unknown
 		
 		for order in order_obj.browse(cr,uid,order_ids):
 			oerp_id = order.id
@@ -116,13 +130,10 @@ class session_connector(osv.osv):
 			'price_unit': float(r['product_item_price']),
 			'product_uom_qty': float(r['product_quantity']),
 			'price_subtotal': float(r['product_final_price']),
-			'virtuemart_order_id': int(r['order_item_id'])
+			'virtuemart_order_id': int(r['virtuemart_order_item_id'])
 			}
 		if not order_line_ids:
-			try:
-				order_line_id = order_line_obj.create(cr,uid,vals_order_line)
-			except Exception, e:
-				raise osv.except_osv("ERROR", "No se pudo insertar una linea en una orden")
+			order_line_id = order_line_obj.create(cr,uid,vals_order_line)
 
 	_logger.info(str(date.today())+" - Interface VirtueMart - Fin carga lineas de ordenes")
 	conn.close()
@@ -134,36 +145,45 @@ class session_connector(osv.osv):
     def update_customers(self,cr,uid,dict_parms):
 	
 	partner_obj = self.pool.get('res.partner')
-	conn = pymysql.connect(host=dict_parms['host'],port=dict_parms['port'],user=dict_parms['user'],\
-		passwd=dict_parms['pwd'],db=dict_parms['dbname'])
+	conn = pymysql.connect(host=dict_parms['host'],user=dict_parms['user'],\
+				passwd=dict_parms['pwd'],db=dict_parms['dbname'])
+	#conn = pymysql.connect(host=dict_parms['host'],port=dict_parms['port'],user=dict_parms['user'],\
+	#	passwd=dict_parms['pwd'],db=dict_parms['dbname'])
 	cur = conn.cursor(pymysql.cursors.DictCursor)
 	
-	cur.execute("select * from jos_vm_user_info")
-	
+	cur.execute("select * from vz3vx_virtuemart_userinfos")
+
+	index = 0	
 	for r in cur.fetchall():
+		print index
+		index = index + 1
+		# import pdb;pdb.set_trace()
 		partner_id = 0
-		partner_id = partner_obj.search(cr,uid,[('virtuemart_user_id','=',r['user_info_id'])])
+		partner_id = partner_obj.search(cr,uid,[('virtuemart_user_id','=',r['virtuemart_user_id'])])
 		if not r['first_name']:
 			r['first_name'] = ''
 		if not r['middle_name']:
 			r['middle_name'] = ''
-
 		vals_partner = {
+				'company_id': 1,
 				'name': r['last_name'].decode('cp1252')+', '+r['first_name'].decode('cp1252')+' '+r['middle_name'].decode('cp1252'),
 				'active': True,
+				'customer': True,
 				'street': r['address_1'].decode('cp1252'),
 				'city': r['city'],
 				'phone': r['phone_1'],
 				'fax': r['fax'],
 				'zip': r['zip'],
-				'email': r['user_email'],	
-				'virtuemart_user_id': r['user_info_id']
+				# 'email': r['user_email'],	
+				'virtuemart_user_id': r['virtuemart_user_id'],
+				'is_company': True,
 				}
 		if not partner_id:
-			try:
-				partner_id = partner_obj.create(cr,uid,vals_partner)
-			except:
-				raise osv.except_osv("ERROR", "No se pudo insertar un cliente")
+			# import pdb;pdb.set_trace()
+			# try:
+			partner_id = partner_obj.create(cr,uid,vals_partner,context = {})
+			# except:
+			# raise osv.except_osv("ERROR", "No se pudo insertar un cliente")
 			
 	_logger.info(str(date.today())+" - Interface VirtueMart - Fin carga clientes")
 	conn.close()
@@ -174,38 +194,43 @@ class session_connector(osv.osv):
 	product_obj = self.pool.get('product.product')
 	template_obj = self.pool.get('product.template')
 
-	conn = pymysql.connect(host=dict_parms['host'],port=dict_parms['port'],user=dict_parms['user'],\
+	conn = pymysql.connect(host=dict_parms['host'],user=dict_parms['user'],\
 		passwd=dict_parms['pwd'],db=dict_parms['dbname'])
+	#conn = pymysql.connect(host=dict_parms['host'],port=dict_parms['port'],user=dict_parms['user'],\
+	#	passwd=dict_parms['pwd'],db=dict_parms['dbname'])
 	cur = conn.cursor(pymysql.cursors.DictCursor)
 	
-	cur.execute("select * from jos_vm_product")
-	
+	cur.execute("select a.*,b.* from vz3vx_virtuemart_products a inner join vz3vx_virtuemart_products_es_es b on a.virtuemart_product_id = b.virtuemart_product_id")
+
+	index = 0	
 	for r in cur.fetchall():
+		# import pdb;pdb.set_trace()
+		_logger.debug('Insertando %s'%r['product_name'].decode('cp1252'))
+		print(index)
+		index = index + 1
 		product_id = 0
 		template_id = 0
 		# product_id = product_obj.search(cr,uid,[('virtuemart_product_id','=',int(r['product_id']))])
-		product_id = product_obj.search(cr,uid,[('default_code','=',r['product_sku'])])
+		product_id = product_obj.search(cr,uid,[('virtuemart_product_id','=',int(r['virtuemart_product_id']))])
+		
 
-		vals_template = {
-				'name': r['product_name'].decode('cp1252'),
-				'description': r['product_desc'].decode('cp1252')
-				}
+		vals = {
+			'categ_id': 1,
+			'cost_method': 'standard',
+			'type': 'consu',
+			'uom_id': 1,
+			'uom_po_id': 1,
+			'default_code': r['product_sku'],
+			'name': r['product_name'].decode('cp1252'),
+			'description': r['product_desc'].decode('cp1252'),
+			'purchase_ok': True,
+			'sale_ok': True,
+			'code': r['virtuemart_product_id'],
+			'active': True,
+			'virtuemart_product_id': int(r['virtuemart_product_id'])
+			}
 		if not product_id:
-			try:
-				template_id = template_obj.create(cr,uid,vals_template)
-				vals_product = {
-					'default_code': r['product_sku'],
-					'code': r['product_id'],
-					'active': True,
-					'product_tmpl_id': template_id,
-					'virtuemart_product_id': int(r['product_id'])
-					}
-				try:
-					product_id = product_obj.create(cr,uid,vals_product)
-				except:
-					raise osv.except_osv("ERROR", "No se pudo insertar un producto")
-			except:
-				raise osv.except_osv("ERROR", "No se pudo insertar un producto")
+			product_id = product_obj.create(cr,uid,vals)
 	conn.close()
 	_logger.info(str(date.today())+" - Interface VirtueMart - Fin carga productos")
 
@@ -224,9 +249,9 @@ class session_connector(osv.osv):
 		dict_connector['port'] = connector.port
 		dict_connector['user'] = connector.user
 		dict_connector['pwd'] = connector.pwd
-		upd_producto = self.update_productos(cr,uid,dict_connector)
-		upd_customers = self.update_customers(cr,uid,dict_connector)
-		ins_orders = self.insert_orders(cr,uid,dict_connector)
+		# upd_producto = self.update_productos(cr,uid,dict_connector)
+		# upd_customers = self.update_customers(cr,uid,dict_connector)
+		# ins_orders = self.insert_orders(cr,uid,dict_connector)
 		ins_order_lines = self.insert_order_lines(cr,uid,dict_connector)
 
 session_connector()
